f1cdc6553463a0876c0791a9aecfa1717c476c00
abelprasad <abelprasad4@gmail.com>
2026-07-17 14:12:42 -0400

    dedupe US_STATES into src/lib/us-states.ts

--- src/components/state-multi-select.tsx
+++ src/components/state-multi-select.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react'
-
-const US_STATES = [
-  'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-  'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-  'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-  'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-  'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC',
-]
+import { US_STATES } from '@/lib/us-states'
 
 interface StateMultiSelectProps {
   selected: string[]
